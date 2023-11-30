36ed9efe968fb7831748df042d7559dafc242d41
haris rehman <harisrehmanchugtai@gmail.com>
2023-11-30 20:22:05 +0500

    Updated stuff

--- frontend-gui/src/components/Search.jsx
+++ frontend-gui/src/components/Search.jsx
@@ -1,22 +1,84 @@
-import React from 'react'
-import "./Search.scss"
-import { MdOutlineMic } from "react-icons/md";
+import React, { useState } from 'react';
+import "./Search.scss" 
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
-const Search = () => {
+const Search = () => { 
+
+  const [searchText, setSearchText] = useState('');
+
+  let handleSpeechEnd = () => {}
+
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition
+  } = useSpeechRecognition({
+    onEnd: handleSpeechEnd, 
+  });
+
+  handleSpeechEnd = () => {
+    setSearchText(transcript);
+  };
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+  
+  const handleSearchChange = (e) => {
+    if (transcript){
+      setSearchText(transcript);
+    }
+    else{
+      setSearchText(e.target.value);
+    } 
+  };
+
+  const clearTranscript = () => {
+    resetTranscript();
+    setSearchText('');
+  };
+
   return (
     <div className="search-container">
       <img src="logo.jpeg" alt="Logo" className="logo" />
-      <div className="search-bar">
-        <input type="text" placeholder="Search..." />
-        <div className="icons">
-          <div className="search-icon">&#128269;</div>
-          <div className="speech-icon">&#128266;</div>
-        </div>
-      </div> 
+ 
+        <div className="search-bar">
+          <input
+            type="text"
+            placeholder="Search..."
+            value={searchText}
+            onChange={handleSearchChange}
+          />
+          <div className="icons">
+            <div
+                className="clear-icon"
+                onClick={clearTranscript}
+                title="Click to clear"
+              >
+                &#10006;
+            </div> 
+            <div className="search-icon">&#128269;</div>
+            <div
+              className="speech-icon"
+              onClick={SpeechRecognition.startListening}
+              title="Click to Speak"
+            >
+              &#128266;
+            </div>
+            {listening && 
+            (
+              <div className="stop-icon"               
+                onClick={SpeechRecognition.startListening}
+                title="Stop Listening">
+              </div>
+            )}
+          </div> 
+        </div>  
       <div className='buttons'>
         <button className="search-button">Search</button>
         <button className="add-article-button">Add Article</button>
-      </div>
+      </div> 
     </div>
   );
 }
