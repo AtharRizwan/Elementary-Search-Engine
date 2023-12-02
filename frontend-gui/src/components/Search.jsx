57c14717d0e0b261de033fbe415a0ade9d36a964
haris rehman <harisrehmanchugtai@gmail.com>
2023-12-02 13:00:51 +0500

    Completed the flask server

--- frontend-gui/src/components/Search.jsx
+++ frontend-gui/src/components/Search.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import "./Search.scss" 
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import axios from 'axios';
 
 const Search = () => { 
-
   const [searchText, setSearchText] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
 
   let handleSpeechEnd = () => {}
 
@@ -39,6 +40,25 @@ const Search = () => {
     setSearchText('');
   };
 
+  const handleSingleWordSearch = async () => {
+    try {
+      const response = await axios.get(`http://localhost:5000/search_1?word=${searchText}`);
+      console.log(response)
+      setSearchResults(response.data);
+    } catch (error) {
+      console.error('Error fetching single word search result:', error);
+    }
+  };
+
+  const handleMultiWordSearch = async () => {
+    try {
+      const response = await axios.get(`http://localhost:5000/search?query=${searchText}`);
+      setSearchResults(response.data);
+    } catch (error) {
+      console.error('Error fetching multi-word search results:', error);
+    }
+  };
+
   return (
     <div className="search-container">
       <img src="logo.jpeg" alt="Logo" className="logo" />
@@ -58,7 +78,7 @@ const Search = () => {
               >
                 &#10006;
             </div> 
-            <div className="search-icon">&#128269;</div>
+            <div className="search-icon" onClick={handleSingleWordSearch} title='Click to search'>&#128269;</div>
             <div
               className="speech-icon"
               onClick={SpeechRecognition.startListening}
@@ -79,6 +99,12 @@ const Search = () => {
         <button className="search-button">Search</button>
         <button className="add-article-button">Add Article</button>
       </div> 
+
+      {searchResults && 
+        (
+          searchResults
+        )
+      }
     </div>
   );
 }
