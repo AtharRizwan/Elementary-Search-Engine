98637510465580916c9ffd59d1eee51387391b5e
Abdullah <ausama.bese22seecs@seecs.edu.pk>
2023-12-03 17:05:22 +0500

    Added stop words functionality

--- forward-index.py
+++ forward-index.py
@@ -2,25 +2,24 @@ import json
 from collections import Counter
 
 # Load JSON data from a file
-with open('abcnews.json', 'r') as file:
+with open('369news.json', 'r') as file:
     data = json.load(file)
 
-# data = data[0]['id']
-# for a in data:
-#     print(a['id'])
 
-
-# content = data[0]['content']
-# content = content.split()
-# content = Counter(content)
-# print(type(content))
 mainList = []
 tempObj = {}
 mainObj = {}
 
+# words to be excluded from the frequency list
+stop_words = {'a', 'an', 'and', 'as', 'at', 'but', 'by', 'for', 'if', 'in', 'into', 'is', 'it', 'no', 'not', 'of',
+    'on', 'or', 'such', 'that', 'the', 'their', 'then', 'there', 'these', 'they', 'this', 'to', 'was',
+    'will', 'with', '.', ',', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '{', '}', '[', ']', 'i','are','can','who'}
+
 for i in data:
-    content = i['content']
-    content = content.split()
+    content = i['content'].split()
+    content = [
+        word for word in content if word.lower() not in stop_words
+    ]
     frequnecy = Counter(content)
     tempObj["id"] = i['id']
     tempObj["author"] = i['author']
@@ -29,12 +28,11 @@ for i in data:
 
     mainList.append(mainObj)
 
-# print(mainList)
+
 
 jsonFormatted = json.dumps(mainList)
 
 with open('test-1.json','w') as outputFile:
-    # print(jsonFormatted)
     outputFile.write(jsonFormatted)
 
 
